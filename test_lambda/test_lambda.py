55a48ab9ff77822b5323f1f0c5583bf525e14530
adriamoya <adriamoyaortiz@gmail.com>
2018-08-08 00:10:26 +0200

    elconfidencial & eleconomista

--- test_lambda/test_lambda.py
+++ test_lambda/test_lambda.py
@@ -3,18 +3,24 @@ from __future__ import print_function
 import datetime
 from crawlers.cincodias import parse_cincodias
 from crawlers.expansion import parse_expansion
+from crawlers.eleconomista import parse_eleconomista
+from crawlers.elconfidencial import parse_elconfidencial
 
 print("Loading function")
 
 def lambda_handler(event, context):
     """Lambda handler for the scraping / downloading process"""
     # Setting the crawl date
-    if event["crawl_date"]:
-        try:
-            crawl_date = datetime.datetime.strptime(event["crawl_date"],"%Y%m%d")
-            print("--> Crawling date %s" % event["crawl_date"])
-        except:
-            print("Wrong input crawl_date. Expecting a YYYYMMDD string, but got a:", event["crawl_date"])
+    # if event["crawl_date"]:
+    #     try:
+    #         crawl_date = datetime.datetime.strptime(event["crawl_date"],"%Y%m%d")
+    #         print("--> Crawling date %s" % event["crawl_date"])
+    #     except:
+    #         print("Wrong input crawl_date. Expecting a YYYYMMDD string, but got a:", event["crawl_date"])
+    
+    crawl_date = datetime.datetime.now()
+    print("--> Crawling date %s" % crawl_date.strftime("%Y-%m-%d"))
+
     # Crawling newspaper
     if event["newspaper"]:
         if event["newspaper"] == "cincodias":
@@ -23,13 +29,18 @@ def lambda_handler(event, context):
         elif event["newspaper"] == "expansion":
             print("--> Crawling expansion")
             parse_expansion(crawl_date)
+        elif event["newspaper"] == "eleconomista":
+            print("--> Crawling eleconomista")
+            parse_eleconomista(crawl_date)
+        elif event["newspaper"] == "elconfidencial":
+            print("--> Crawling elconfidencial")
+            parse_elconfidencial(crawl_date)
         else:
             print("Expecting the following newspapers: cincodias, expansion, eleconomista, elconfidenicial.")
 
 if __name__ == "__main__":
     event = {
         "crawl_date": "20180720",
-        "newspaper": "expansion"
-
+        "newspaper": "elconfidencial"
     }
     lambda_handler(event, {})
