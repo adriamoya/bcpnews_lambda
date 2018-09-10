f3a9f960b140fe8a8a0e1e25812f67317c035cb1
adriamoya <adriamoyaortiz@gmail.com>
2018-09-10 18:56:15 +0200

    Lauching EC2 from Lambda

--- crawl_lambda/crawl.py
+++ crawl_lambda/crawl.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
+import boto3
 import datetime
 from crawlers.cincodias import parse_cincodias
 from crawlers.elconfidencial import parse_elconfidencial
@@ -10,8 +11,17 @@ print("Loading function")
 
 def lambda_handler(event, context):
     """Lambda handler for the scraping / downloading process"""
+
+    REGION = 'eu-central-1' # region to launch instance.
+    INSTANCE_ID = 'i-02ad64e2ab12d8719'
+    INSTANCE_TYPE = 't2.medium' # instance type to launch.
+
+    ec2 = boto3.resource('ec2')
+    instance = ec2.Instance(INSTANCE_ID)
+
     crawl_date = datetime.datetime.now()
     print("--> Crawling date %s" % crawl_date.strftime("%Y-%m-%d"))
+
     try:
         trigger_file = event['Records'][0]['s3']['object']['key']
         if trigger_file:
@@ -27,40 +37,10 @@ def lambda_handler(event, context):
             elif 'eleconomista' in trigger_file:
                 print("--> Crawling Expansion")
                 parse_expansion(crawl_date)
+            elif 'expansion' in trigger_file:
+                print("--> Launching EC2...")
+                response = instance.start()
     except:
         # A cloudwatch event will trigger the first crawler
         print("--> Crawling cincodias")
         parse_cincodias(crawl_date)
-
-
-if __name__ == "__main__":
-    event = {
-        "crawl_date": "20180720",
-        "newspaper": "elconfidencial"
-    }
-    lambda_handler(event, {})
-
-    # Setting the crawl date
-    # if event["crawl_date"]:
-    #     try:
-    #         crawl_date = datetime.datetime.strptime(event["crawl_date"],"%Y%m%d")
-    #         print("--> Crawling date %s" % event["crawl_date"])
-    #     except:
-    #         print("Wrong input crawl_date. Expecting a YYYYMMDD string, but got a:", event["crawl_date"])
-
-    # Crawling newspaper
-    # if event["newspaper"]:
-    #     if event["newspaper"] == "cincodias":
-    #         print("--> Crawling cincodias")
-    #         parse_cincodias(crawl_date)
-    #     elif event["newspaper"] == "expansion":
-    #         print("--> Crawling expansion")
-    #         parse_expansion(crawl_date)
-    #     elif event["newspaper"] == "eleconomista":
-    #         print("--> Crawling eleconomista")
-    #         parse_eleconomista(crawl_date)
-    #     elif event["newspaper"] == "elconfidencial":
-    #         print("--> Crawling elconfidencial")
-    #         parse_elconfidencial(crawl_date)
-    #     else:
-    #         print("Expecting the following newspapers: cincodias, expansion, eleconomista, elconfidenicial.")
